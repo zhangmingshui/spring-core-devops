50784d945f26a73bdc0d2b65abb219250d24d8a6
steve chong <dr.steve.chong@gmail.com>
2020-03-02 16:46:24 +0000

    trigger

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,6 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		// added comment
 
 	}
 }
